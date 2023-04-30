7dc68d59084b49c82d4eb40d7ebe5dff54599ef5
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-04-30 20:56:32 +0200

    fetch assets, log output

--- script.sh
+++ script.sh
@@ -17,10 +17,16 @@ function main ()
 
     fetchRepoInfo $repo $repo/info.json
     fetchReleaseList $repo $repo/releases.json
-    # loop through releases and fetch assets
 
-    # TODO loop through releases
-    #   TODO fetch assets and store in $repo/assets/$tagName.json
+    # loop through releases and fetch assets
+    mkdir -p $repo/assets
+    while read tagName; do
+      if [ -z "$tagName" ]; then
+        break
+      fi
+
+      fetchAssetsList $repo "$tagName" "$repo/assets/$tagName.json"
+    done <<< "$(cat ${repo}/releases.json | jq -r ".[].tagName")"
 
     # TODO combine $repo/assets/* and $repo/releases.json into connector.json
     # TODO combine info.json and connector.json into connector.json
@@ -69,11 +75,17 @@ function fetchDependents ()
   local -r FILE_OUT=${3}
 
   # get repos depending on the SDK
+  log "Fetching dependents..."
   gh dependents $REPO > dependents.tmp
 
   # dependents without excluded
+  log "Removing excluded dependents..."
   jq -s -r "[.[0].dependents[] | .user + \"/\" + .repo] - .[1] | .[]" \
     dependents.tmp $EXCLUDED > $FILE_OUT
+
+  DEPENDENTS_COUNT_ALL=`jq ".dependents | length" dependents.tmp`
+  DEPENDENTS_COUNT_FILTERED=`printf %d $(wc -l <$FILE_OUT)`
+  log "Fetched $DEPENDENTS_COUNT_FILTERED dependents (excluded $(($DEPENDENTS_COUNT_ALL-$DEPENDENTS_COUNT_FILTERED)))"
 
   # remove temporary file
   rm dependents.tmp
@@ -92,6 +104,7 @@ function fetchRepoInfo ()
   local -r FILE_OUT=${2}
 
   # get basic repo info
+  log "Fetching info for $REPO..."
   gh repo view $REPO \
     --json nameWithOwner,description,createdAt,url,stargazerCount,forkCount \
     > $FILE_OUT
@@ -111,8 +124,33 @@ function fetchReleaseList ()
   local -r FILE_OUT=${2}
 
   # get list of releases and transform it into json
+  log "Fetching releases for $REPO..."
   gh release list --repo $REPO | \
     jq --raw-input --slurp 'split("\n") | map(split("\t")) | .[0:-1] | map( { "tagName": .[0], "type": .[1] } )' \
+    > $FILE_OUT
+}
+
+#---------------------------------------------------------------------------------------------------
+# Fetch list of assets for a release
+#
+# Parameters
+# 1 Repository name (e.g. conduitio/conduit-connector-file)
+# 2 Release tag (e.g. v0.1.0)
+# 3 Output file
+#---------------------------------------------------------------------------------------------------
+function fetchAssetsList ()
+{
+  local -r REPO=${1}
+  local -r TAG_NAME=${2}
+  local -r FILE_OUT=${3}
+
+  # get list of assets and transform it into json
+
+  # gh release view - available fields:
+  # apiUrl,assets,author,body,createdAt,id,isDraft,isPrerelease,name,publishedAt,tagName,tarballUrl,targetCommitish,uploadUrl,url,zipballUrl
+  log "Fetching assets for $REPO@$TAG_NAME..."
+  gh release view $TAG_NAME --repo $REPO \
+    --json assets,body,isDraft,isPrerelease,name,publishedAt,tagName,url \
     > $FILE_OUT
 }
 
@@ -144,6 +182,16 @@ function gh ()
 
   # execute gh command
   command gh $@
+}
+
+#---------------------------------------------------------------------------------------------------
+# Log a message to stderr
+#
+# All parameters will be printed as a string using printf
+#---------------------------------------------------------------------------------------------------
+function log ()
+{
+  printf "%s\n" "$*" >&2;
 }
 
 # run script
